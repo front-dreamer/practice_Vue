407f3f111ed8d6b0f492ccb24b3b547970d64921
sturtle333 <sturtle333@gmail.com>
2019-08-23 22:46:16 +0900

    Download vues

---
+++ practice/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+    <b-nav-item disabled><b>Practice</b></b-nav-item>
+    <b-nav-item disabled>Counter: 1/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ practice/src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        Some question here?
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        list of answer
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="seccess" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
