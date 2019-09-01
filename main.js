5df528fc0ba32ef96754e01d001c6aa7627386d1
sturtle333 <sturtle333@gmail.com>
2019-09-01 23:12:34 +0900

    2019.09.01 vue.set and svg

--- main.js
+++ main.js
@@ -4,15 +4,25 @@ var app = new Vue({
     message: 'Hello, Vue.js!',
     list: ['사과','망고','딸기'],
     show: false,
+    isFucked: false,
+    messageColor:'black',
+    bgcolor:'white',
+    radius: 50,
   },
   methods: {
     handleClick: function(event) {
       alert(event.target);
       if(this.show == true) {
         this.show = false;
+        this.isFucked = false;
+        this.bgcolor = 'white';
+        this.messageColor = 'black';
       }
       else {
         this.show = true;
+        this.isFucked = true;
+        this.bgcolor = 'red';
+        this.messageColor = 'blue';
       }
     }
   }
