fac9c51739e6ab7c7faa0936524c31354988a515
sturtle333 <sturtle333@gmail.com>
2019-09-17 14:26:13 +0900

    template

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-var app = new Vue({
+Vue.component('my-component', {
+  template: '<div>{{ message }}<br>{{ second }}</div>',
+  data: function(){
+    return{
+      message: 'MyTemplate',
+      second: 'second'
+    }
+  }
+})
+
+new Vue({
   el: '#app',
   data: {
-    message: 'Hello, Vue.js!',
-    list: ['사과','망고','딸기'],
-    show: false,
-    isFucked: false,
-    messageColor:'black',
-    bgcolor:'white',
-    radius: 50,
-  },
-  methods: {
-    handleClick: function(event) {
-      alert(event.target);
-      if(this.show == true) {
-        this.show = false;
-        this.isFucked = false;
-        this.bgcolor = 'white';
-        this.messageColor = 'black';
-      }
-      else {
-        this.show = true;
-        this.isFucked = true;
-        this.bgcolor = 'red';
-        this.messageColor = 'blue';
-      }
-    }
+    message: 'Hello, World!'
   }
 })
